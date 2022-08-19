345e69739331cb27f9060dcf9e8272a638ecb8a1
mazyn <gilmarfilho75@gmail.com>
2022-08-19 18:53:04 -0300

    feat: :sparkles: add scroll position restauration when navigating between pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,6 +26,7 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes, {
       initialNavigation: 'enabledBlocking',
+      scrollPositionRestoration: 'enabled',
     }),
   ],
   exports: [RouterModule],
